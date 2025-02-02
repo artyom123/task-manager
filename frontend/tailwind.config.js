3d7ab4e48a7c95f81e86d43b3504a9414342463d
Artsem Virshych <artyom.virshych@gmail.com>
2025-02-02 21:56:54 +0200

    init vite + vue and tailwind

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+    theme: {
+      extend: {
+        colors: {
+          primary: "#3B82F6",
+          secondary: "#EC4899",
+          lightGray: "#F3F4F6",
+        },
+      },
+    },
+    plugins: [],
+  };
